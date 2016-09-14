a69a0f88f127d6709bd89eec59b9c48ce8343ef7
Your Name <newuser@example.com>
2016-09-14 09:33:17 +0700

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  
+
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-  get 'signup'  => 'users#new'
+  get 'signup'  => 'static_pages#signup'
 
 
 
